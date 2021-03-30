81c99f067e5879f1ec044e21d59d27d2383cc261
Nevsimao <coday.ink@gmail.com>
2021-03-30 18:08:06 -0400

    database working

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-// const dbuser = 'boat-backend'
-// const password = 'GlI3KD3vXU03zTFB'
 const dbuser = 'admin';
 const password = '1234test';
 
-// const mongoURI = `mongodb+srv://${dbuser}:${password}@cluster0.4u8fy.mongodb.net/boat-backend?retryWrites=true&w=majority`;
 const mongoURI = `mongodb+srv://admin:1234test@cluster0.nbzrd.mongodb.net/boat-backend?retryWrites=true&w=majority`;
 
 mongoose
